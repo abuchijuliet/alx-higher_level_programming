5708a3f9e8c1a233ea41ee9c2ac0b5e3f6643507
abuchijuliet <abuchijuliet44@gmail.com>
2023-01-23 15:04:30 +0000

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,6 @@ def safe_print_list(my_list=[], x=0):
     for m in range(x):
         try:
             print(my_list[m], end="")
-
             n += 1
         except IndexError:
             break
