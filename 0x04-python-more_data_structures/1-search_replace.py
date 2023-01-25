50bf948b8b459e277b335c41aed7ebd82cd35778
abuchijuliet <abuchijuliet44@gmail.com>
2023-01-25 09:47:50 +0000

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    def s_r_elm(elm):
-        return (elm if elm != search else replace)
-    return list(map(s_r_elm, my_list))
+    if len(my_list) == 0:
+        return my_list
+
+    new_lst = [elem if elem != search else replace for elem in my_list]
+    return new_lst
--- 0x04-python-more_data_structures/README.md
+++ 0x04-python-more_data_structures/README.md
@@ -1,1 +1,2 @@
-0x04. Python - More Data Structures: Set, Dictionary
+More data structures tasks
+Data structure More structure
